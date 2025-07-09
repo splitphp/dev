9f059103ee27fab36a568e808f77ad28bb134e3d
Gabriel Valentoni Guelfi <gabriel.valguelfi@gmail.com>
2025-07-09 01:00:27 -0300

    upd core

--- application/routes/site.php
+++ application/routes/site.php
@@ -2,8 +2,8 @@
 
 namespace Application\Routes;
 
-use \SplitPHP\WebService;
-use \SplitPHP\Request;
+use SplitPHP\WebService;
+use SplitPHP\Exceptions;
 
 class Site extends WebService
 {
@@ -12,7 +12,8 @@ class Site extends WebService
     $this->setAntiXsrfValidation(false);
 
     // Home Page Endpoints:
-    $this->addEndpoint(['POST','PUT'], '/home/?test?', function ($input) {
+    $this->addEndpoint(['GET', 'POST', 'PUT'], '/home', function ($input) {
+      throw new Exceptions\BadRequest("This is a test endpoint for POST and PUT requests.");
 
       return $this->response
         ->withStatus(200)
