b23049ce19b4cbbee7254a13ea0106c67703d99e
Gabriel Valentoni Guelfi <gabriel.valguelfi@gmail.com>
2025-07-11 22:22:13 -0300

    .

--- modules/log/dbmigrations/1750190495_create-table-logrecord.php
+++ modules/log/dbmigrations/1750190495_create-table-logrecord.php
@@ -13,6 +13,7 @@ class CreateTableLogrecord extends Migration
       ->id('id_log_record')
       ->string('ds_key', 17)
       ->datetime('dt_log')->setDefaultValue(DbVocab::SQL_CURTIMESTAMP())
+      ->string('ds_filepath')
       ->string('ds_context', 100)
       ->text('tx_message')
       ->Index('KEY', DbVocab::IDX_UNIQUE)->onColumn('ds_key');
